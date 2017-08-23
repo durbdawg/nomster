958626ffffdb4fdc9624ef7933811ae1a12914d7
durbdawg <nick.durbin@gmail.com>
2017-08-23 17:23:39 +0000

    Added some styling to the place page, arranged for the comments to report the last comment submitted, adjusted some code to fix bugs, but having an issue submitting comments.

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 20170823022541) do
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
