1a5a3e66e6556c340263174e07eff3e0361f40ad
durbdawg <nick.durbin@gmail.com>
2017-08-25 21:13:12 +0000

    Added a slider with images to the index page of the application. Will need toworkon some styling.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,9 @@ Rails.application.configure do
    #  password: ENV['GMAIL_PASSWORD']
    # }
 
-   config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :test
 
-    config.action_mailer.default_url_options = { host: 'localhost:3030'}
+  config.action_mailer.default_url_options = { host: 'localhost:3030'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
